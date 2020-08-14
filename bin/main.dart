aa5699f05578d21fe44277e4e1210ad41c181c7c
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2020-08-15 00:36:02 +0500

    validate if no args

--- bin/main.dart
+++ bin/main.dart
@@ -7,22 +7,29 @@ extension StringExtension on String {
 }
 
 void main(List<String> arguments) async {
+  if (arguments.isEmpty) {
+    print('Please give view name');
+    return;
+  }
   var dir = arguments[0];
   var dirPath = 'lib/ui/views/$dir';
-  if(!File('pubspec.yaml').existsSync()){
+  if (!File('pubspec.yaml').existsSync()) {
     print('This command is only supported in a dart or flutter project root');
   }
   await Directory(dirPath).create(recursive: true);
   var viewFile = await File('$dirPath/${dir.capitalize()}View.dart').create();
-  var modelFile = await File('$dirPath/${dir.capitalize()}ViewModel.dart').create();
+  var modelFile =
+      await File('$dirPath/${dir.capitalize()}ViewModel.dart').create();
   var viewTemplate = readTplFile('View');
   var viewModelTemplate = readTplFile('ViewModel');
-  
-  viewFile.writeAsStringSync(viewTemplate.replaceAll('{{CLASS_NAME}}', '${dir.capitalize()}'));
-  modelFile.writeAsStringSync(viewModelTemplate.replaceAll('{{CLASS_NAME}}', '${dir.capitalize()}'));
+
+  viewFile.writeAsStringSync(
+      viewTemplate.replaceAll('{{CLASS_NAME}}', '${dir.capitalize()}'));
+  modelFile.writeAsStringSync(
+      viewModelTemplate.replaceAll('{{CLASS_NAME}}', '${dir.capitalize()}'));
 }
 
-String readTplFile(String fileName){
+String readTplFile(String fileName) {
   var tplPath = Platform.script.path;
   var tplSplit = tplPath.split('/');
   tplSplit.removeLast();
